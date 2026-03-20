b536926ee7a827595050c64c301d660508256874
Saku Karttunen <saku.karttunen@gmail.com>
2026-03-20 13:45:12 +0200

    small fixes to urls

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite"
+import { resolve } from "path"
 
 export default defineConfig({
-  root: "src"
+  root: "src",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/product.html")
+      }
+    },
+  }
 })
