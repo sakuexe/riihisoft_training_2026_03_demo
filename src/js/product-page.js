66cc550cdd1d47bb88e1913a68d23493ccf96459
Saku Karttunen <saku.karttunen@gmail.com>
2026-03-20 12:50:53 +0200

    updated the images to coffee beans

---
+++ src/js/product-page.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    name: "Rubber duck debuggers",
+    price: 14.99,
+    image: "classico_beans.webp"
+  },
+  {
+    name: "Curly brace classics",
+    price: 16.99,
+    image: "milano_beans.webp"
+  },
+  {
+    name: "Hotfix express",
+    price: 18.99,
+    image: "crema_beans.webp"
+  },
+  {
+    name: "Midnight commit edition",
+    price: 19.99,
+    image: "starbucks_beans.webp"
+  },
+];
+
+const breadcrumb = document.querySelector(".breadcrumb > :last-child");
+const title = document.querySelector("h1");
+const price = document.querySelector(".price");
+const image = document.querySelector("figure img");
